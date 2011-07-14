1308f6fde43722844b8b0e68a4bec337335e5e4a
Cameron Pulsford <cpulsfor@gmail.com>
2011-07-14 10:59:08 -0400

    Added some informal tests

--- lll/main.m
+++ lll/main.m
@@ -9,6 +9,8 @@
 #import <Cocoa/Cocoa.h>
 #import "Symbol.h"
 #import "PersistentList.h"
+#import "LispReader.h"
+#import "NullAdditions.h"
 
 int main(int argc, char *argv[])
 {
@@ -27,6 +29,12 @@ int main(int argc, char *argv[])
     pl = [pl cons:@"yo yo!"];
     
     NSLog(@"CBP____ %@", pl);
+    
+    NSLog(@"CBP____ %@", [LispReader readString:@"((:hello (worldworld 1 2 3 4 '5)) world :hi '(mom))"]);
+    
+    NSLog(@"CBP____ %@", [LispReader readString:@"~'''`()"]);
+    
+    NSLog(@"CBP____ %@", [LispReader readString:@"(nil 1 1 2 3 4 5 6)"]);
     
     [pool release];
 
